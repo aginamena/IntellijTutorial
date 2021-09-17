9cde14347f97dd866e1548036c14a18da4336f3b
aginamena5 <aginamena5@gmail.com>
2021-09-16 22:17:10 -0400

    added main method

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -19,5 +19,14 @@ public class AddressBook {
     }
     public static void main(String[] args){
         System.out.println("Address book");
+        BuddyInfo buddy  = new BuddyInfo("Tom", "Carleton", "613");
+        BuddyInfo buddy2  = new BuddyInfo("Tom2", "Carleton", "613");
+        BuddyInfo buddy3 = new BuddyInfo("Tom3", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.addBuddy(buddy2);
+        addressBook.addBuddy(buddy3);
+        addressBook.removeBuddy(buddy3);
+
     }
 }
